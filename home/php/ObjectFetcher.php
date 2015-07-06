21fd9800a639d51a07332a1f9da9f864a7a273af
Andrea Marzoli <andrea.m993@gmail.com>
2015-07-06 13:39:47 +0200

    ANDREA

--- home/php/ObjectFetcher.php
+++ home/php/ObjectFetcher.php
@@ -5,9 +5,11 @@ class ObjectFetcher implements Fetcher {
     public function fetch($con, $thingToFetch) {
         $result = mysqli_query($con, "SELECT * FROM Oggetto WHERE ID='" . $thingToFetch . "'");
 
+        $all = array();
         while ($row = mysqli_fetch_array($result)) {
-            echo $row['Object'];
+            array_push($all, $row);
         }
+        return json_encode($all);
     }
 
 }
