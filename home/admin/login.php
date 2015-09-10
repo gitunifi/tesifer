133171047d083c31c12ebd653eb3b2f7147b72cb
Andrea Marzoli <andrea.m993@gmail.com>
2015-09-10 20:53:14 +0200

    ANDREA

--- home/admin/login.php
+++ home/admin/login.php
@@ -22,7 +22,7 @@
     function login(){
         if($("#username").val().trim() != "" && $("#password").val().trim() != ""){
             $.post( "index.php", {username: $("#username").val(), password: $("#password").val()},function( ok ) {
-                console.log(ok == "false");
+                console.log(ok == false);
                 if(ok == "false"){
                     $(".errLogin").css({"display":"inherit"});
                     setTimeout(function() {$(".errLogin").hide();}, 6000);
