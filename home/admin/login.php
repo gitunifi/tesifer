e3808b55069e513ab887c09cf6d7c5c31128a0a0
Andrea Marzoli <andrea.m993@gmail.com>
2015-09-10 20:56:00 +0200

    ANDREA

--- home/admin/login.php
+++ home/admin/login.php
@@ -1,40 +1,4 @@
-<script src="components/jquery/dist/jquery.min.js"></script>
-<div id="popUpLogin" class="popUp">
-    <div id="formAutenticazione" class="formAutenticazione">
-        <h3>Effettua il login</h3>
-        <form method="post" id="formLogin" class="formLogin" onsubmit="login();return false;">
-            <input type="hidden" value="login" name="action">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" name="username" placeholder="Inserisci il tuo Username">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Inserisci la password">
-            </div>
-            <p style="display: none;" class="bg-danger errLogin">Password o Username errati!</p>
-            <input id="btnLogin" class="btn btn-primary btn-lg btn-block btnLogin" type="submit" value="Login">
-        </form>
-    </div>
-</div>
-
-<script>
-    function login(){
-        if($("#username").val().trim() != "" && $("#password").val().trim() != ""){
-            $.post( "index.php", {username: $("#username").val(), password: $("#password").val()},function( ok ) {
-                if(ok == false){
-                    $(".errLogin").css({"display":"inherit"});
-                    setTimeout(function() {$(".errLogin").hide();}, 6000);
-                } else {
-                    location.href = "index.php";
-                }
-            }, "json");
-        } else {
-            $(".errLogin").css({"display":"inherit"});
-            setTimeout(function() {$(".errLogin").hide();}, 6000);
-        }
-    }
-</script>
+<link href="components/bootstrap/dist/css/bootstrap.min.css">
 <style>
     body {
         padding-top: 0px;
@@ -600,3 +564,40 @@
         margin: 0px;
     }
 </style>
+<script src="components/jquery/dist/jquery.min.js"></script>
+<div id="popUpLogin" class="popUp">
+    <div id="formAutenticazione" class="formAutenticazione">
+        <h3>Effettua il login</h3>
+        <form method="post" id="formLogin" class="formLogin" onsubmit="login();return false;">
+            <input type="hidden" value="login" name="action">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Inserisci il tuo Username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Inserisci la password">
+            </div>
+            <p style="display: none;" class="bg-danger errLogin">Password o Username errati!</p>
+            <input id="btnLogin" class="btn btn-primary btn-lg btn-block btnLogin" type="submit" value="Login">
+        </form>
+    </div>
+</div>
+
+<script>
+    function login(){
+        if($("#username").val().trim() != "" && $("#password").val().trim() != ""){
+            $.post( "index.php", {username: $("#username").val(), password: $("#password").val()},function( ok ) {
+                if(ok == false){
+                    $(".errLogin").css({"display":"inherit"});
+                    setTimeout(function() {$(".errLogin").hide();}, 6000);
+                } else {
+                    location.href = "index.php";
+                }
+            }, "json");
+        } else {
+            $(".errLogin").css({"display":"inherit"});
+            setTimeout(function() {$(".errLogin").hide();}, 6000);
+        }
+    }
+</script>
